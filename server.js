03a992e852844e8f2e433b265f1fd81ff80802de
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-10 00:11:18 -0500

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -2,19 +2,23 @@
 const express = require('express');
 const app = express();
 
-// app.get('/greeting/:name', (req, res) => {
-// 	res.send({params: req.params, queries: req.query})
-// });
-
 app.get('/greeting', (req, res) => {
 	res.send("Hello stranger")
 });
 
 app.get('/greeting/:name', (req, res) => {
 	res.send("Hello there " + req.params.name + "!");
-
-	// console.log(req.params.name);
 });
+
+
+// Tip App
+app.get('/tip', (req, res) => {
+	res.send("Hello")
+})
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+	res.send(`${req.params.tipPercentage * req.params.total / 100}`);
+	// res.send((req.params.tipPercentage / 100) * req.params.total);
+})
 
 app.listen(2000, () => {
 	"listening 2000..."
